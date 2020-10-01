40e05eeee6748c2e19e22a5909c857b8ae81f0b2
sakshatl <voteforsakshat@gmail.com>
2020-10-01 18:07:00 +0530

    edited hacktober homepage

--- src/components/Hacktober/Hacktober.js
+++ src/components/Hacktober/Hacktober.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import HeroBanner from './components/HeroBanner';
 import Projects from './components/Projects'
+import Navbar from '../Home/components/Navbar'
 
 const Hactober = () => {
   return ( 
     <div>
+      <Navbar />
       <HeroBanner />
       <Projects />
     </div>
